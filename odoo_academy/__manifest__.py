710af627e60e9d08e9766c131099c8c3a164c7c5
Isaac Benitez <ibe@odoo.com>
2020-10-21 00:19:24 +0000

    [ADD] Basic module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage training""",
+    'description': """Academy app to manage training""",
+    'author': 'Isaac Benitez',
+    'website': 'https://odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ],
+}
