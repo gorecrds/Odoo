ee2ae86ef931e111a801848e2ec5fb814cc259ac
Isaac Benitez <ibe@odoo.com>
2020-10-28 23:28:27 +0000

    Added security and menu

--- odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -2,16 +2,27 @@
 
 {
     'name': 'Odoo Academy',
+    
     'summary': """Academy app to manage training""",
+    
     'description': """Academy app to manage training""",
+    
     'author': 'Isaac Benitez',
+    
     'website': 'https://odoo.com',
+    
     'category': 'Training',
+    
     'version': '0.1',
+    
     'depends': ['base'],
+    
     'data': [
-        
+        'security/academy_security.xml',
+        'security/ir.model.access.csv',
+        'views/academy_menuitems.xml',
     ],
+    
     'demo': [
         'demo/academy_demo.xml',
     ],
